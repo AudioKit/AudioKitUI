363ce83a36593b3dabe0393bd5d73422a7481111
Aurelius Prochazka <aurephd@gmail.com>
2021-02-06 16:03:08 -0800

    Extension for detecting previews

--- Sources/AudioKitUI/AudioKitUI.swift
+++ Sources/AudioKitUI/AudioKitUI.swift
@@ -15,3 +15,13 @@ public extension Color {
         return CrossPlatformColor(self).cgColor
     }
 }
+
+public extension EnvironmentValues {
+    var isPreview: Bool {
+        #if DEBUG
+        return ProcessInfo.processInfo.environment["XCODE_RUNNING_FOR_PREVIEWS"] == "1"
+        #else
+        return false
+        #endif
+    }
+}
