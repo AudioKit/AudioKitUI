a5ed4557a9e1ee94c811a778376ee054a087fc68
Aurelius Prochazka <aurephd@gmail.com>
2021-02-11 12:20:28 -0800

    Attaching to v5-main

--- Package.swift
+++ Package.swift
@@ -14,7 +14,7 @@ let package = Package(
             targets: ["AudioKitUI"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/AudioKit/AudioKit.git", .branch("v5-develop"))
+        .package(url: "https://github.com/AudioKit/AudioKit.git", .branch("v5-main"))
     ],
     targets: [
         .target(
