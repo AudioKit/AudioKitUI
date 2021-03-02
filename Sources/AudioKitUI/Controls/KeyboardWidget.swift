97b361e03b3e2d056fa0c31beabf3c0813e8cac3
Aurelius Prochazka <aurephd@gmail.com>
2021-03-02 13:44:55 -0800

    Trying to make KeyboardWidget cross platform

--- Sources/AudioKitUI/Controls/KeyboardWidget.swift
+++ Sources/AudioKitUI/Controls/KeyboardWidget.swift
@@ -1,7 +1,7 @@
 import AudioKit
 import SwiftUI
 
-public struct KeyboardWidget: UIViewRepresentable {
+public struct KeyboardWidget: ViewRepresentable {
 
     public var firstOctave = 2
     public var octaveCount = 2
@@ -9,17 +9,26 @@ public struct KeyboardWidget: UIViewRepresentable {
     public typealias UIViewType = KeyboardView
     public var delegate: KeyboardDelegate?
 
-    public func makeUIView(context: Context) -> KeyboardView {
+    #if os(macOS)
+    public func makeNSView(context: Context) -> KeyboardView {
         let view = KeyboardView()
         view.delegate = delegate
         view.firstOctave = firstOctave
         view.octaveCount = octaveCount
         return view
     }
+    public func updateNSView(_ nsView: KeyboardView, context: Context) {}
+    #else
+    public func makeUIView(context: Context) -> KeyboardView {
 
-    public func updateUIView(_ uiView: KeyboardView, context: Context) {
-        //
+        let view = KeyboardView()
+        view.delegate = delegate
+        view.firstOctave = firstOctave
+        view.octaveCount = octaveCount
+        return view
     }
+    public func updateUIView(_ uiView: KeyboardView, context: Context) {}
+    #endif
 
     public init(delegate: KeyboardDelegate? = nil, firstOctave: Int = 2, octaveCount: Int = 2) {
         self.delegate = delegate
