a9767296c9591290059fb3997dbdb8f7238bb6e7
Matt Pfeiffer <mattpfeiffer54@gmail.com>
2021-05-03 09:33:29 -0500

    Added WavetableArrayView and made WavetableView public

--- Sources/AudioKitUI/Visualizations/WavetableView.swift
+++ Sources/AudioKitUI/Visualizations/WavetableView.swift
@@ -3,11 +3,13 @@
 import AudioKit
 import SwiftUI
 
-class WavetableModel: ObservableObject {
-    @Published var floats: [Float] = Table(.sine).content
+public class WavetableModel: ObservableObject {
+    @Published public var floats: [Float] = Table(.sine).content
     var node: DynamicOscillator?
     
-    func updateNode(_ node: DynamicOscillator) {
+    public init(){}
+    
+    public func updateNode(_ node: DynamicOscillator) {
         if node !== self.node {
             self.node = node
             self.node!.wavetableUpdateHandler = setFloats
@@ -16,18 +18,28 @@ class WavetableModel: ObservableObject {
     }
     
     func setFloats(floats: [Float]) {
-        self.floats = floats
+        self.floats = floats.downSample(to: 128)
+            //downSampleFloats(floats: floats, numberOfOutputSamples: 128)
     }
 }
 
-struct WavetableView: View {
-    @StateObject var wavetableModel = WavetableModel()
-    @State var strokeColor = Color.white
-    @State var fillColor = Color.green.opacity(0.8)
-    @State var backgroundColor = Color.black
+public struct WavetableView: View {
     var node: DynamicOscillator
+    @StateObject var wavetableModel = WavetableModel()
+    private var strokeColor : Color
+    private var strokeLineWidth: CGFloat
+    private var fillColor : Color
+    private var backgroundColor : Color
     
-    var body: some View {
+    public init(_ node: DynamicOscillator, strokeColor: Color = Color.white, strokeLineWidth: CGFloat = 1.0, fillColor: Color = Color.green.opacity(0.8), backgroundColor: Color = Color.black) {
+        self.node = node
+        self.strokeColor = strokeColor
+        self.strokeLineWidth = strokeLineWidth
+        self.fillColor = fillColor
+        self.backgroundColor = backgroundColor
+    }
+    
+    public var body: some View {
         return GeometryReader { geometry in
             createWave(width: geometry.size.width, height: geometry.size.height)
         }
@@ -64,13 +76,13 @@ struct WavetableView: View {
             Path { path in
                 path.addLines(points)
             }
-            .stroke(strokeColor, lineWidth: 3)
+            .stroke(strokeColor, lineWidth: strokeLineWidth)
         }
     }
 }
 
 struct WavetableView_Previews: PreviewProvider {
     static var previews: some View {
-        WavetableView(node: DynamicOscillator())
+        WavetableView(DynamicOscillator())
     }
 }
