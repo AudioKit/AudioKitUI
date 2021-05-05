2ebf38e34c4c9f837ae65190c04fa13984d2f914
Aurelius Prochazka <aurephd@gmail.com>
2021-05-04 22:08:10 -0700

    Updated for branch name change

--- Package.swift
+++ Package.swift
@@ -14,7 +14,7 @@ let package = Package(
             targets: ["AudioKitUI"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/AudioKit/AudioKit.git", .branch("v5-main"))
+        .package(url: "https://github.com/AudioKit/AudioKit.git", .branch("main"))
     ],
     targets: [
         .target(
