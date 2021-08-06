cf633f32aa0fdfd7d903ae69139655b43c333906
Evan Murray <evanmurray22@gmail.com>
2021-08-05 23:47:54 -0500

    Add KeyboardWidgetSettings
    These changes allow the wrapped KeyboardView to update certain properties through SwiftUI when needed.

--- Sources/AudioKitUI/Controls/KeyboardWidget.swift
+++ Sources/AudioKitUI/Controls/KeyboardWidget.swift
@@ -1,10 +1,23 @@
+// Copyright AudioKit. All Rights Reserved. Revision History at http://github.com/AudioKit/AudioKitUI/
+
 import AudioKit
 import SwiftUI
 
+/// Settings class for the KeyboardWidget - Publishes to KeyboardView
+public class KeyboardWidgetSettings: ObservableObject {
+    /// Octave Count for the wrapped KeyboardView
+    @Published public var octaveCount: Int = 2
+    /// First Octave for the wrapped KeyboardView
+    @Published public var firstOctave: Int = 0
+    /// Polyphonic Mode toggle for the wrapped KeyboardView
+    @Published public var polyphonicMode: Bool = false
+    public init() {}
+}
+/// SwiftUI View - Wraps a KeyboardView
 public struct KeyboardWidget: ViewRepresentable {
-
-    public var firstOctave = 2
-    public var octaveCount = 2
+    var firstOctave: Int
+    var octaveCount: Int
+    var polyphonicMode: Int
 
     public typealias UIViewType = KeyboardView
     public var delegate: KeyboardDelegate?
@@ -15,24 +28,36 @@ public struct KeyboardWidget: ViewRepresentable {
         view.delegate = delegate
         view.firstOctave = firstOctave
         view.octaveCount = octaveCount
+        view.polyphonicMode = polyphonicMode
         return view
     }
-    public func updateNSView(_ nsView: KeyboardView, context: Context) {}
+    public func updateNSView(_ nsView: KeyboardView, context: Context) {
+        nsView.firstOctave = firstOctave
+        nsView.octaveCount = octaveCount
+        nsView.polyphonicMode = polyphonicMode
+        view.setNeedsDisplay()
+    }
     #else
     public func makeUIView(context: Context) -> KeyboardView {
-
         let view = KeyboardView()
         view.delegate = delegate
         view.firstOctave = firstOctave
         view.octaveCount = octaveCount
+        view.polyphonicMode = polyphonicMode
         return view
     }
-    public func updateUIView(_ uiView: KeyboardView, context: Context) {}
+    public func updateUIView(_ uiView: KeyboardView, context: Context) {
+        uiView.firstOctave = firstOctave
+        uiView.octaveCount = octaveCount
+        uiView.polyphonicMode = polyphonicMode
+        uiView.setNeedsDisplay()
+    }
     #endif
 
-    public init(delegate: KeyboardDelegate? = nil, firstOctave: Int = 2, octaveCount: Int = 2) {
+    public init(delegate: KeyboardDelegate? = nil, firstOctave: Int, octaveCount: Int) {
         self.delegate = delegate
         self.firstOctave = firstOctave
         self.octaveCount = octaveCount
+        self.polyphonicMode = polyphonicMode
     }
 }
