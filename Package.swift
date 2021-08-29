2308a4c8fc1977c5bd8d9d428f6e5807e07ca4a5
Aurelius Prochazka <aurephd@gmail.com>
2021-08-28 22:44:32 -0700

    Use develop AudioKit on the develop branch of AudioKitUI

--- Package.swift
+++ Package.swift
@@ -7,7 +7,7 @@ let package = Package(
     name: "AudioKitUI",
     platforms: [ .macOS(.v11), .iOS(.v14)],
     products: [.library(name: "AudioKitUI", targets: ["AudioKitUI"])],
-    dependencies: [.package(url: "https://github.com/AudioKit/AudioKit.git", from: "5.2.0")],
+    dependencies: [.package(url: "https://github.com/AudioKit/AudioKit.git", .branch("develop"))],
     targets: [
         .target(name: "AudioKitUI", dependencies: ["AudioKit"]),
         .testTarget(name: "AudioKitUITests", dependencies: ["AudioKitUI"]),
