8deeb7c743c2dab554abb5aee55404e42c593eb4
Aurelius Prochazka <aurephd@gmail.com>
2022-01-19 22:43:39 -0800

    Initial version of stepper controls

---
+++ Sources/AudioKitUI/Controls/TempoDraggableStepper.swift
@@ -0,0 +1,39 @@
+import SwiftUI
+
+public struct TempoDraggableStepper: View {
+    @Binding var tempo: Float
+    var sensitivty: Float = 1.0
+
+    public init(tempo: Binding<Float>) {
+        _tempo = tempo
+    }
+
+    @State var initialTouchPoint: CGPoint?
+    @State var initialTempo: Float = 0.0
+
+    public var body: some View {
+        GeometryReader { geo in
+            let font = Font.system(size: geo.size.height * 0.6, weight: .light)
+            HStack {
+                Text("TEMPO ").fontWeight(.bold).font(font)
+                Text("◀").font(font).onTapGesture { tempo -= 1 }
+                Text("\(Int(tempo))").font(font).frame(width: geo.size.height * 1.5)
+                    .gesture(DragGesture(minimumDistance: 0, coordinateSpace: .global)
+                                .onChanged({ touch in
+                        if let initialTouchPoint = initialTouchPoint {
+                            tempo = max(1, initialTempo + Float(touch.location.x - initialTouchPoint.x + (initialTouchPoint.y - touch.location.y)) * sensitivty / 30.0)
+                        } else {
+                            initialTouchPoint = touch.location
+                            initialTempo = tempo
+                        }
+                    }).onEnded({ _ in
+                        initialTouchPoint = nil
+                        initialTempo = 0.0
+                    }) )
+
+                Text("▶").font(font).onTapGesture { tempo += 1 }
+                Spacer()
+            }.frame(width: geo.size.width, height: geo.size.height)
+        }
+    }
+}
