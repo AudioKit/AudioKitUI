c33ec09bac2bafece77eef2b2d876ad071b73869
Taylor Holliday <taylor@audulus.com>
2022-04-17 13:44:52 -0700

    draw notes

--- Sources/AudioKitUI/Controls/PianoRoll.swift
+++ Sources/AudioKitUI/Controls/PianoRoll.swift
@@ -2,7 +2,7 @@
 
 import SwiftUI
 
-struct PianoRollNote {
+struct PianoRollNote: Equatable, Hashable {
 
     /// The start step.
     var start: Int
@@ -28,34 +28,59 @@ struct PianoRollModel {
 
 }
 
+struct PianoRollNoteView: View {
+    @Binding var note: PianoRollNote
+    var gridSize: CGSize
+
+    var body: some View {
+        Rectangle()
+            .cornerRadius(5.0)
+            .foregroundColor(.cyan)
+            .frame(width: gridSize.width * CGFloat(note.length),
+                   height: gridSize.height)
+            .offset(x: gridSize.width * CGFloat(note.start),
+                    y: gridSize.height * CGFloat(note.pitch))
+    }
+}
+
 struct PianoRoll: View {
 
     @Binding var model: PianoRollModel
 
     var body: some View {
-        Canvas { cx, size in
-            var x: CGFloat = 0
-            for _ in 0 ... model.length {
+        ZStack {
+            Canvas { cx, size in
+                var x: CGFloat = 0
+                for _ in 0 ... model.length {
 
-                var path = Path()
-                path.move(to: CGPoint(x: x, y: 0))
-                path.addLine(to: CGPoint(x: x, y: size.height))
+                    var path = Path()
+                    path.move(to: CGPoint(x: x, y: 0))
+                    path.addLine(to: CGPoint(x: x, y: size.height))
 
-                cx.stroke(path, with: .color(.gray), lineWidth: 1)
+                    cx.stroke(path, with: .color(.gray), lineWidth: 1)
 
-                x += size.width / CGFloat(model.length)
+                    x += size.width / CGFloat(model.length)
+                }
+
+                var y: CGFloat = 0
+                for _ in 0 ... model.height {
+
+                    var path = Path()
+                    path.move(to: CGPoint(x: 0, y: y))
+                    path.addLine(to: CGPoint(x: size.width, y: y))
+
+                    cx.stroke(path, with: .color(.gray), lineWidth: 1)
+
+                    y += size.height / CGFloat(model.height)
+                }
             }
-
-            var y: CGFloat = 0
-            for _ in 0 ... model.height {
-
-                var path = Path()
-                path.move(to: CGPoint(x: 0, y: y))
-                path.addLine(to: CGPoint(x: size.width, y: y))
-
-                cx.stroke(path, with: .color(.gray), lineWidth: 1)
-
-                y += size.height / CGFloat(model.height)
+            GeometryReader { proxy in
+                ForEach(model.notes, id: \.self) { note in
+                    PianoRollNoteView(
+                        note: $model.notes[model.notes.firstIndex(of: note)!],
+                        gridSize: CGSize(width: proxy.size.width / CGFloat(model.length),
+                                         height: proxy.size.height / CGFloat(model.height)))
+                }
             }
         }
     }
@@ -63,7 +88,10 @@ struct PianoRoll: View {
 
 struct PianoRollTestView: View {
 
-    @State var model = PianoRollModel(notes: [], length: 16, height: 16)
+    @State var model = PianoRollModel(notes: [
+        PianoRollNote(start: 1, length: 2, pitch: 3),
+        PianoRollNote(start: 5, length: 1, pitch: 4)
+    ], length: 16, height: 16)
 
     var body: some View {
         PianoRoll(model: $model).padding()
