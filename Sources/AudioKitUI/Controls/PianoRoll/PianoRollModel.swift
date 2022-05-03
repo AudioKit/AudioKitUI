8001fa304cbd08f695519d9c9b3f012d52392c47
Taylor Holliday <taylor@audulus.com>
2022-05-02 19:50:45 -0700

    Add comment.

--- Sources/AudioKitUI/Controls/PianoRoll/PianoRollModel.swift
+++ Sources/AudioKitUI/Controls/PianoRoll/PianoRollModel.swift
@@ -22,6 +22,10 @@ public struct PianoRollNote: Equatable, Identifiable {
 
 }
 
+/// Value oriented data model for `PianoRoll`
+///
+/// The data model is abstracted away from MIDI so that it's up to you
+/// to determine what pitch means (e.g. how it is scale quantized).
 public struct PianoRollModel: Equatable {
     public init(notes: [PianoRollNote], length: Int, height: Int) {
         self.notes = notes
