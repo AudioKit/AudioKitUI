5fea4e2cc7b8a52c49db665ef707394c4aa121a7
Aurelius Prochazka <aurephd@gmail.com>
2022-09-11 01:17:46 -0700

    Public initializer

--- Sources/AudioKitUI/Controls/ADSRWidget.swift
+++ Sources/AudioKitUI/Controls/ADSRWidget.swift
@@ -7,6 +7,10 @@ public struct ADSRWidget: UIViewRepresentable {
     public typealias UIViewType = ADSRView
     var callback: (AUValue, AUValue, AUValue, AUValue) -> Void
 
+    public init(callback: @escaping (AUValue, AUValue, AUValue, AUValue) -> Void) {
+        self.callback = callback
+    }
+
     public func makeUIView(context _: Context) -> ADSRView {
         let view = ADSRView(callback: callback)
         view.bgColor = .systemBackground
