7492e3cefb6c9bff790cb3b94979e446a6a8984a
mahal raskin <mahal@raskinapps.ch>
2024-03-12 12:47:27 +0100

    fixed hound linter warnings. no semantics, just syntax.

--- Sources/AudioKitUI/Visualizations/SpectrogramFlatView/UIColor+intermediate.swift
+++ Sources/AudioKitUI/Visualizations/SpectrogramFlatView/UIColor+intermediate.swift
@@ -17,22 +17,22 @@ extension Array where Element: UIColor {
             let firstIndex = Int(approxIndex.rounded(.down))
             let secondIndex = Int(approxIndex.rounded(.up))
             let fallbackIndex = Int(approxIndex.rounded())
-            
+
             let firstColor = self[firstIndex]
             let secondColor = self[secondIndex]
             let fallbackColor = self[fallbackIndex]
-            
-            var (r1, g1, b1, a1): (CGFloat, CGFloat, CGFloat, CGFloat) = (0, 0, 0, 0)
-            var (r2, g2, b2, a2): (CGFloat, CGFloat, CGFloat, CGFloat) = (0, 0, 0, 0)
-            guard firstColor.getRed(&r1, green: &g1, blue: &b1, alpha: &a1) else { return fallbackColor }
-            guard secondColor.getRed(&r2, green: &g2, blue: &b2, alpha: &a2) else { return fallbackColor }
-            
+
+            var (red1, green1, blue1, alpha1): (CGFloat, CGFloat, CGFloat, CGFloat) = (0, 0, 0, 0)
+            var (red2, green2, blue2, alpha2): (CGFloat, CGFloat, CGFloat, CGFloat) = (0, 0, 0, 0)
+            guard firstColor.getRed(&red1, green: &green1, blue: &blue1, alpha: &alpha1) else { return fallbackColor }
+            guard secondColor.getRed(&red2, green: &green2, blue: &blue2, alpha: &alpha2) else { return fallbackColor }
+
             let intermediatePercentage = approxIndex - CGFloat(firstIndex)
             return UIColor(
-                red: CGFloat(r1 + (r2 - r1) * intermediatePercentage),
-                green: CGFloat(g1 + (g2 - g1) * intermediatePercentage),
-                blue: CGFloat(b1 + (b2 - b1) * intermediatePercentage),
-                alpha: CGFloat(a1 + (a2 - a1) * intermediatePercentage)
+                red: CGFloat(red1 + (red2 - red1) * intermediatePercentage),
+                green: CGFloat(green1 + (green2 - green1) * intermediatePercentage),
+                blue: CGFloat(blue1 + (blue2 - blue1) * intermediatePercentage),
+                alpha: CGFloat(alpha1 + (alpha2 - alpha1) * intermediatePercentage)
             )
         }
     }
